2d7ea23c6f9ec27a247398eee286142ef173a143
dongjin2008 <80329234+dongjin2008@users.noreply.github.com>
2023-11-08 07:05:23 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html, js}"],
+  theme: {
+    colors: {
+      'primary': "#2A3443",
+      'secondary': "#D7D7D0",
+      'accent': "#C99B63"
+    },
+    fontSize: {
+      'xl': "76px",
+      'lg': "49px",
+      'base': "39px",
+      'sm': "25px",
+      'xs': "20px"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
