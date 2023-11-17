48805ea0354e6ba0d538ac0def89ed74dd19e43f
dongjin2008 <80329234+dongjin2008@users.noreply.github.com>
2023-11-17 22:15:56 +0100

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css'
 
 const poppins = Poppins({ subsets: ['latin'], display: 'swap', variable: '--font-poppins', weight: ['200', '300', '400', '500', '600', '700', '800'] });
@@ -20,7 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
