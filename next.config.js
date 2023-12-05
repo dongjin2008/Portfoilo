26628fd6f00f07f2d94a5311ac652aeddd98a771
dongjin2008 <gimdongjin2008@gmail.com>
2023-12-05 21:24:59 +0000

    Added blog cms

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: "https://starter-kit-gules.vercel.app/blog"
+      },
+      {
+        source: "/blog/:path*",
+        destination: "https://starter-kit-gules.vercel.app/blog/:path*"
+      },
+    ];
+  },
+}
+
+
 
 module.exports = nextConfig
